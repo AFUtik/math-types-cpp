2c52867a6efdb68a609a23d3614790d30a49ba92
AFUtik <Coffecraft38@gmail.com>
2025-02-26 22:50:56 +0200

    Reference allowed

--- include/mtp/matrix.hpp
+++ include/mtp/matrix.hpp
@@ -6,38 +6,66 @@
 
 namespace mtp {
 
-template <typename T, std::size_t N, std::size_t M>
+template <typename T, std::size_t N, std::size_t M = N>
 struct matrix : public DataContainer<T, N*M> {
     static_assert(N!=0 || M!=0, "Matrix size can't be zero.");
 
-    static constexpr std::size_t n = N;
-    static constexpr std::size_t m = M;
+    //static constexpr std::size_t n = N;
+    //static constexpr std::size_t m = M;
 
     // vector<T*, N> rows[M];
 
     using DataContainer<T, N*M>::DataContainer;
 
     constexpr matrix(const DataContainer<T, N*M>& container) {
-        std::copy(container.data, container.data + this->SIZE, this->data);
+        std::copy(container.data, container.data + this->size, this->data);
     }
 
-    /* UTILS methods */
-
-    template<typename CastType>
-    static constexpr inline matrix<CastType, N, M> cast(const matrix<T, N, M> &container) {
-        matrix<CastType, N, M> new_container;
-        for(std::size_t i = 0; i < N; i++) new_container.data[i] = static_cast<CastType>(container.data[i]);
-        return new_container;
+    /**
+    * @brief Gets an object by xy cordinates.
+    * @param x Width
+    * @param y Height
+    */
+    constexpr inline const T& get(const size_t& x, const size_t& y) {
+        return this->data[y*N+x];
     }
 
-    template<std::size_t n, std::size_t m>
-    static constexpr inline matrix<T, n, m> resize(const matrix<T, N, M> &mat) {
-        matrix<T, n, m> new_container;
-        
-        for(std::size_t i = 0; i < std::min(n * m, N * M); i++) new_container.data[i] = mat.data[i];
-        return new_container;
+    /** 
+    * @brief The product of matrix and vector.
+    * @return Returns a multiplied vector by matrix.
+    */
+    constexpr inline vector<T, N> operator*(const vector<T, N>& vec) {
+        vector<T, N> new_vector;
+        for (size_t i = 0; i < M; i++) {
+            for (size_t j = 0; j < N; j++) new_vector.data[i]+=vec.data[j]*this->data[i * N + j];
+        }
+        return new_vector;
+    }
+};
+
+
+template <typename T, std::size_t W, std::size_t H = W, std::size_t V = H>
+struct matrix3D : public DataContainer<T, W*H*V> {
+    static_assert(W!=0 || H!=0 || V!=0, "Matrix size can't be zero.");
+    static constexpr size_t WH = W*H; /* The size of the layer of 3d matrix. */
+
+    using DataContainer<T, W*H*V>::DataContainer;
+
+    constexpr matrix3D(const DataContainer<T, W*H*V>& container) {
+        std::copy(container.data, container.data + this->size, this->data);
     }
 
+    /**
+    * @brief Gets an object by xyz cordinates.
+    * @param x Width
+    * @param y Height
+    * @param z Volume
+    */
+    constexpr inline const T& get(const size_t& x, const size_t& y, const size_t& z) {
+        static_assert(x<=W || y<=H || z<=V, "The cordinates exceed the limits of 3d matrix.");
+
+        return this->data[y*WH+z*W+x];
+    }
 };
 
 using matrix4x3 = matrix<float, 4, 3>;
